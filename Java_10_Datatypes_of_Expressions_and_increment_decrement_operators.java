46013776672b706f7f685f8fc9d927942de98077
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-05-12 18:47:38 +0530

    Add files via upload

---
+++ Java_10_Datatypes_of_Expressions_and_increment_decrement_operators.java
@@ -0,0 +1,56 @@
+public class Java_10_Datatypes_of_Expressions_and_increment_decrement_operators {
+    public static void main(String[] args) {
+//        byte a=1;
+//        short b=2;
+//        int c=a+b;
+//
+//        short d=5;
+//        int e=6;
+//        int f=d+e;
+//
+//        long g=300;
+//        float h=3.2F;
+//        float i=g+h;
+//
+//        int j=15;
+//        float k=3.2F;
+//        float l=j+k;
+//
+//        char m='A';
+//        int n=15;
+//        int o=m+n;
+//
+//        char p='B';
+//        short q=10;
+//        int r=p+q;
+//
+//        long s=15;
+//        double t=5.4;
+//        double u=s+t;
+//
+//        float v=3.4F;
+//        double w=5.566;
+//        double x=v+w;
+//
+//        System.out.println(c+" "+f+" "+i+" "+l+" "+o+" "+r+" "+u+" "+x);
+        //output 3 11 303.2 18.2 80 76 20.4 8.96600009536743
+
+        //Increment and Decrement Operators
+        int a=56;
+        int b=72;
+        System.out.println(a);//56
+        System.out.println(a++);//56
+        System.out.println(a);//57
+        System.out.println(++a);//58
+        System.out.println(a);//58
+        char c='A';
+        System.out.println(c++);//A
+        System.out.println(c);//B
+        System.out.println(++c);//C
+
+
+
+
+
+    }
+}
