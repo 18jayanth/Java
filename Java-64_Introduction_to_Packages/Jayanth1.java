52070e0dc5bb7d5541e7577dcfd76d6dbffd18ce
Tulugu Jayanth <87133065+18jayanth@users.noreply.github.com>
2025-05-19 17:41:32 +0530

    Add files via upload

---
+++ Java-64_Introduction_to_Packages/Jayanth1.java
@@ -0,0 +1,8 @@
+package Java_64_Introduction_to_packages;
+//packages will group related classes
+//you cannot run it directly with javac filename.java there will be another process
+public class Jayanth1 {
+    public static void main(String[] args) {
+        System.out.println("this is jayanth1");
+    }
+}
---
+++ Java-64_Introduction_to_Packages/Jayanth2.java
@@ -0,0 +1,7 @@
+package Java_64_Introduction_to_packages;
+
+public class Jayanth2 {
+    public static void main(String[] args) {
+        System.out.println("this is jayanth2");
+    }
+}
